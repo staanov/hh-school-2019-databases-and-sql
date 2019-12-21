716818276aeaf254815ad1e82bbb3041bd9684df
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2019-12-21 20:11:06 +0300

    Fixed GROUP BY in task-5

--- task_5.sql
+++ task_5.sql
@@ -1,15 +1,15 @@
-SELECT required_info.name
-FROM
-(SELECT name, v.vacancy_id, SUM(
+SELECT vb.name
+FROM 
+(SELECT v.vacancy_id, v.vacancy_body_id, SUM(
 CASE WHEN r.vacancy_id IS NULL THEN 0
 	ELSE 1 END) AS sum_response
-FROM vacancy_body AS vb
-INNER JOIN vacancy AS v
-ON v.vacancy_body_id = vb.vacancy_body_id
+FROM vacancy AS v
 LEFT JOIN response AS r
 ON v.vacancy_id = r.vacancy_id
 WHERE (r.vacancy_id IS NULL) OR ((r.response_time - v.creation_time) <= '7 days'::interval)
-GROUP BY v.vacancy_id, name) AS required_info
-WHERE required_info.sum_response < 5
-GROUP BY required_info.name
-ORDER BY required_info.name;
+GROUP BY v.vacancy_id, v.vacancy_body_id) AS required_info
+INNER JOIN vacancy_body AS vb
+ON required_info.vacancy_body_id = vb.vacancy_body_id
+WHERE sum_response < 5
+GROUP BY vb.vacancy_body_id, vb.name
+ORDER BY vb.name;
