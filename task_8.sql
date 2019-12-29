db8099c644238f0c6f8aeda7674d2f74c61b743b
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2019-12-29 12:21:19 +0300

    Task-8 was done

---
+++ task_8.sql
@@ -0,0 +1,28 @@
+CREATE TABLE resume_changes (
+    resume_changes_id serial PRIMARY KEY,
+    resume_id integer NOT NULL,
+    last_change_time timestamp NOT NULL,
+    json jsonb NOT NULL,
+    FOREIGN KEY (resume_id) REFERENCES resume(resume_id)
+);
+
+CREATE FUNCTION save_changes() RETURNS trigger AS $save_changes$
+    BEGIN
+        IF NEW IS NULL THEN
+            RETURN OLD;
+        END IF;
+        INSERT INTO resume_changes (resume_id, last_change_time, json)
+        VALUES (OLD.resume_id, now(), to_json(old.*));
+    END;
+$save_changes$ LANGUAGE plpgsql;
+
+CREATE TRIGGER save_changes BEFORE UPDATE OR DELETE ON resume 
+FOR EACH ROW EXECUTE FUNCTION save_changes();
+
+SELECT
+    resume_id,
+    last_change_time,
+    json
+FROM resume_changes
+WHERE resume_id = 1
+ORDER BY resume_id, last_change_time;
